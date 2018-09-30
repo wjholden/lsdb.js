a2b4165c14d1c923c57460e132ffbdb6970bc661
William John Holden <wjholden@gmail.com>
2018-09-30 08:39:10 -0400

    Tests pass in Node.js

--- lsdb.js
+++ lsdb.js
@@ -26,16 +26,58 @@ function split_snmpwalk_output(s) {
 }
 
 function parseLsa(lsa) {
-    var link_state_type = getByte(lsa, 3);
-    var link_state_id = getInt(lsa, 4);
-    var advertising_rtr = getInt(lsa, 8);
-    var length = getShort(lsa, 18);
-    return link_state_type;
+    switch (getByte(lsa, 3)) {
+    case 1:
+	return parseRouterLsa(lsa);
+    case 2:
+	return parseNetworkLsa(lsa);
+    default:
+	return null;
+    }
 }
 
 function parseRouterLsa(lsa) {
     var number_of_links = getShort(lsa, 22);
-    
+    var g = "";
+    var advertising_router = intToIp(getInt(lsa, 8));
+    for (var i = 0 ; i < number_of_links ; i++) {
+	var offset = 6 * 4 + (3 * 4 * i);
+	var link_id = intToIp(getInt(lsa, offset));
+	var link_data = intToIp(getInt(lsa, offset + 4));
+	var link_type = getByte(lsa, offset + 8);
+	var metric = getShort(lsa, offset + 10);
+	switch(link_type) {
+	case 1: // point-to-point
+	case 2: // transit network
+	    g += "\"" + advertising_router + "\" -> \"" + link_id +
+		"\" [label=" + 	metric + "];\n";
+	    break;
+	case 3: // stub network
+	    g += "\"" + advertising_router + "\" -> \"" + link_id + "/" +
+		link_data + "\" [label=" + metric + "];\n";
+	    break;
+	}
+    }
+    return g;
+}
+
+function parseNetworkLsa(lsa) {
+    var length = getShort(lsa, 18);
+    var link_id = intToIp(getInt(lsa, 4));
+    var netmask = intToIp(getInt(lsa, 20));
+    var position = 24;
+    var g = "";
+    while (position < length) {
+	var attached_router = intToIp(getInt(lsa, position));
+	g += "\"" + link_id + "/" + netmask + "\" -> \"" +
+	    attached_router + "\";\n";
+	position += 4;
+    }
+    return g;
+}
+
+function intToIp(x) {
+    return (x >>> 24) + "." + ((x >>> 16) & 0xff) + "." + ((x >>> 8) & 0xff) + "." + (x & 0xff);
 }
 
 function getByte(buffer, offset) {
@@ -52,43 +94,4 @@ function getInt(buffer, offset) {
 
 function getBytes(buffer, offset, byteCount) {
     return parseInt(buffer.substring(2 * offset, 2 * (offset + byteCount)), 16);
-}
-
-
-var input = `iso.3.6.1.2.1.14.4.1.8.0.0.0.0.1.192.168.1.1.192.168.1.1 = Hex-STRING: 00 00 22 01 C0 A8 01 01 C0 A8 01 01 80 00 04 98
-55 69 00 48 02 00 00 04 C0 A8 01 01 FF FF FF FF
-03 00 00 22 C0 A8 01 02 C0 A8 FE 01 01 00 1A 6D
-C0 A8 FE 00 FF FF FF 00 03 00 1A 6D C0 A8 FF 02
-C0 A8 FF 01 02 00 0A 18
-iso.3.6.1.2.1.14.4.1.8.0.0.0.0.1.192.168.1.2.192.168.1.2 = Hex-STRING: 00 01 22 01 C0 A8 01 02 C0 A8 01 02 80 00 01 85
-C7 1E 00 78 00 00 00 08 C0 A8 01 02 FF FF FF FF
-03 00 00 01 C0 A8 64 64 C0 A8 64 01 02 00 00 01
-C0 A8 14 00 FF FF FF 00 03 00 00 01 C0 A8 01 01
-C0 A8 FE 02 01 00 02 62 C0 A8 FE 00 FF FF FF 00
-03 00 02 62 C0 A8 FF 02 C0 A8 FF 02 02 00 03 DB
-C0 A8 FD 03 C0 A8 FD 02 02 00 00 90 C0 A8 0A 00
-FF FF FF 00 03 00 00 01
-iso.3.6.1.2.1.14.4.1.8.0.0.0.0.1.192.168.1.3.192.168.1.3 = Hex-STRING: 00 02 22 01 C0 A8 01 03 C0 A8 01 03 80 00 00 EF
-FE 97 00 48 00 00 00 04 C0 A8 64 64 C0 A8 64 02
-02 00 00 01 C0 A8 01 03 FF FF FF FF 03 00 00 01
-C0 A8 FD 03 C0 A8 FD 03 02 00 B5 20 C0 A8 1E 00
-FF FF FF 00 03 00 00 01
-iso.3.6.1.2.1.14.4.1.8.0.0.0.0.1.192.168.1.100.192.168.1.100 = Hex-STRING: 00 02 22 01 C0 A8 01 64 C0 A8 01 64 80 00 00 EA
-7F E6 00 30 01 00 00 02 C0 A8 01 64 FF FF FF FF
-03 00 00 01 C0 A8 64 64 C0 A8 64 64 02 00 00 01
-iso.3.6.1.2.1.14.4.1.8.0.0.0.0.2.192.168.100.100.192.168.1.100 = Hex-STRING: 00 02 22 02 C0 A8 64 64 C0 A8 01 64 80 00 00 E8
-72 32 00 24 FF FF FF 00 C0 A8 01 64 C0 A8 01 02
-C0 A8 01 03
-iso.3.6.1.2.1.14.4.1.8.0.0.0.0.2.192.168.253.3.192.168.1.3 = Hex-STRING: 00 02 22 02 C0 A8 FD 03 C0 A8 01 03 80 00 00 E6
-FF A2 00 20 FF FF FF 00 C0 A8 01 03 C0 A8 01 02
-iso.3.6.1.2.1.14.4.1.8.0.0.0.0.2.192.168.255.2.192.168.1.2 = Hex-STRING: 00 01 22 02 C0 A8 FF 02 C0 A8 01 02 80 00 01 2B
-56 09 00 20 FF FF FF 00 C0 A8 01 02 C0 A8 01 01
-iso.3.6.1.2.1.14.4.1.8.0.0.0.0.5.0.0.0.0.192.168.1.1 = Hex-STRING: 00 00 20 05 00 00 00 00 C0 A8 01 01 80 00 01 58
-70 7E 00 24 00 00 00 00 80 00 00 01 00 00 00 00
-00 00 00 01`;
-
-var a = split_snmpwalk_output(input);
-console.log(a);
-for (var i = 0 ; i < a.length ; i++) {
-    console.log(parseLsa(a[i]));
 }
