76c450d23f7888d390cb682b5eae154e1720923d
William John Holden <wjholden@gmail.com>
2019-09-06 08:00:21 +0900

    Major updates

--- lsdb.js
+++ lsdb.js
@@ -1,38 +1,43 @@
+var lsdbConfig = {
+    showRouterLsa: true,
+    showNetworkLsa: true
+};
+
 function split_snmpwalk_output(s) {
     // This function is a finite state machine.
     // State 0: Discard the current line and continue until we see a line containing " = Hex-STRING: ".
     // State 1: We see the magic string, telling us to format and add the parsed string to the LSDB (if any)
     // and begin parsing the next string.
     // State 2: This line does not contain the magic string and we should append its value to the parsed value.
-    
+
     var lines = s.split(/\n/);
     var state = 0;
     var lsdb = [];
     var s = "";
 
     for (var i = 0; i < lines.length; i++) {
-	if (lines[i].includes(" = Hex-STRING: ")) {
-	    if (state == 2) {
-		lsdb.push(s.replace(/ /g, ''));
-	    }
-	    state = 1;
-	    s = lines[i].substring(15 + lines[i].lastIndexOf(" = Hex-STRING: "));
-	    state = 2;
-	} else if (state == 2) {
-	    s += lines[i];
-	}
+        if (lines[i].includes(" = Hex-STRING: ")) {
+            if (state == 2) {
+                lsdb.push(s.replace(/ /g, ''));
+            }
+            state = 1;
+            s = lines[i].substring(15 + lines[i].lastIndexOf(" = Hex-STRING: "));
+            state = 2;
+        } else if (state == 2) {
+            s += lines[i];
+        }
     }
-    return(lsdb);
+    return (lsdb);
 }
 
 function parseLsa(lsa) {
     switch (getByte(lsa, 3)) {
-    case 1:
-	return parseRouterLsa(lsa);
-    case 2:
-	return parseNetworkLsa(lsa);
-    default:
-	return "";
+        case 1:
+            return lsdbConfig.showRouterLsa ? parseRouterLsa(lsa) : "";
+        case 2:
+            return lsdbConfig.showNetworkLsa ? parseNetworkLsa(lsa) : "";
+        default:
+            return "";
     }
 }
 
@@ -40,23 +45,24 @@ function parseRouterLsa(lsa) {
     var number_of_links = getShort(lsa, 22);
     var g = "";
     var advertising_router = intToIp(getInt(lsa, 8));
-    for (var i = 0 ; i < number_of_links ; i++) {
-	var offset = 6 * 4 + (3 * 4 * i);
-	var link_id = intToIp(getInt(lsa, offset));
-	var link_data = intToIp(getInt(lsa, offset + 4));
-	var link_type = getByte(lsa, offset + 8);
-	var metric = getShort(lsa, offset + 10);
-	switch(link_type) {
-	case 1: // point-to-point
-	case 2: // transit network
-	    g += "\"" + advertising_router + "\" -> \"" + link_id +
-		"\" [label=" + 	metric + "];\n";
-	    break;
-	case 3: // stub network
-	    g += "\"" + advertising_router + "\" -> \"" + link_id + "/" +
-		link_data + "\" [label=" + metric + "];\n";
-	    break;
-	}
+    for (var i = 0; i < number_of_links; i++) {
+        var offset = 6 * 4 + (3 * 4 * i);
+        var link_id = intToIp(getInt(lsa, offset));
+        var link_data = intToIp(getInt(lsa, offset + 4));
+        var link_type = getByte(lsa, offset + 8);
+        var metric = getShort(lsa, offset + 10);
+        switch (link_type) {
+            case 1: /* fall through */
+            // point-to-point
+            case 2: // transit network
+                g += "\"" + advertising_router + "\" -> \"" + link_id +
+                    "\" [label=" + metric + "];\n";
+                break;
+            case 3: // stub network
+                g += "\"" + advertising_router + "\" -> \"" + link_id + "/\n" +
+                    link_data + "\" [label=" + metric + "];\n";
+                break;
+        }
     }
     return g;
 }
@@ -68,10 +74,10 @@ function parseNetworkLsa(lsa) {
     var position = 24;
     var g = "";
     while (position < length) {
-	var attached_router = intToIp(getInt(lsa, position));
-	g += "\"" + link_id + "/" + netmask + "\" -> \"" +
-	    attached_router + "\";\n";
-	position += 4;
+        var attached_router = intToIp(getInt(lsa, position));
+        g += "\"" + link_id + "/\n" + netmask + "\" -> \"" +
+            attached_router + "\";\n";
+        position += 4;
     }
     return g;
 }
